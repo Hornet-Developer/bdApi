Hornet-Developer/bdApi

commit c9546017a75b9873f04b264e3a6e28b3e7dd934d

[xenforo] fixed json view renderer internal param filtering

xenforo/library/bdApi/ViewRenderer/Json.php
@@ -71,20 +71,20 @@ class bdApi_ViewRenderer_Json extends XenForo_ViewRenderer_Json
         return $params;
     }
 
-    public static function jsonEncodeForOutput($input, $addDefaultParams = true)
+    public function renderViewObject($class, $responseType, array &$params = array(), &$templateName = '')
     {
-        if ($addDefaultParams) {
-            self::_addDefaultParams($input);
-        }
+        $return = parent::renderViewObject($class, $responseType, $params, $templateName);
 
-        foreach (array_keys($input) as $inputKey) {
-            if (strpos($inputKey, '_') === 0) {
-                // filter out internal params
-                unset($input[$inputKey]);
+        if ($return === null) {
+            foreach (array_keys($params) as $paramKey) {
+                if (strpos($paramKey, '_') === 0) {
+                    // filter out internal params
+                    unset($params[$paramKey]);
+                }
             }
         }
 
-        return XenForo_ViewRenderer_Json::jsonEncodeForOutput($input, false);
+        return $return;
     }
 
     protected static function _addDefaultParams(array &$params = array())

commit 8852e8c6ad82cd2a30d24746a6d03c8cf46d1eac

[xenforo] fixed bug missing default json params introduced in c954601

xenforo/library/bdApi/ViewRenderer/Json.php
@@ -71,6 +71,15 @@ class bdApi_ViewRenderer_Json extends XenForo_ViewRenderer_Json
         return $params;
     }
 
+    public static function jsonEncodeForOutput($input, $addDefaultParams = true)
+    {
+        if ($addDefaultParams) {
+            self::_addDefaultParams($input);
+        }
+
+        return XenForo_ViewRenderer_Json::jsonEncodeForOutput($input, false);
+    }
+
     public function renderViewObject($class, $responseType, array &$params = array(), &$templateName = '')
     {
         $return = parent::renderViewObject($class, $responseType, $params, $templateName);