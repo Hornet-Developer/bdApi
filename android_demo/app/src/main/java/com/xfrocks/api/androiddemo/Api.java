Hornet-Developer/bdApi

commit 4e5a0fed06866a23c56338c7db0f7a4a4dbaa732

[android_demo] added support for avatar upload

android_demo/app/src/main/java/com/xfrocks/api/androiddemo/Api.java
@@ -11,10 +11,17 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.xfrocks.api.androiddemo.persist.Row;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.InputStreamBody;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -36,6 +43,7 @@ public class Api {
     public static final String URL_INDEX = "index";
     public static final String URL_USERS = "users";
     public static final String URL_USERS_ME = "users/me";
+    public static final String URL_USERS_ME_AVATAR = "users/me/avatar";
     public static final String URL_TOOLS_LOGIN_SOCIAL = "tools/login/social";
 
     public static final String URL_OAUTH_TOKEN_PARAM_GRANT_TYPE = "grant_type";
@@ -60,6 +68,8 @@ public class Api {
     public static final String URL_USERS_PARAM_DOB_DAY = "user_dob_day";
     public static final String URL_USERS_PARAM_EXTRA_DATA = "extra_data";
     public static final String URL_USERS_PARAM_EXTRA_TIMESTAMP = "extra_timestamp";
+
+    public static final String URL_USERS_ME_AVATAR_PARAM_AVATAR = "avatar";
 
     public static AccessToken makeAccessToken(JSONObject response) {
         try {
@@ -172,11 +182,17 @@ public class Api {
 
     public static String makeAuthorizeUri(String redirectTo) {
         try {
+            String authorizeRedirectUri = makeAuthorizeRedirectUri(redirectTo);
+            String encodedRedirectTo = "";
+            if (authorizeRedirectUri != null) {
+                URLEncoder.encode(authorizeRedirectUri, "UTF-8");
+            }
+
             return String.format(
                     "%s/index.php?oauth/authorize/&client_id=%s&redirect_uri=%s&response_type=code&scope=%s",
                     BuildConfig.API_ROOT,
                     URLEncoder.encode(BuildConfig.CLIENT_ID, "UTF-8"),
-                    URLEncoder.encode(makeAuthorizeRedirectUri(redirectTo), "UTF-8"),
+                    encodedRedirectTo,
                     URLEncoder.encode("read", "UTF-8")
             );
         } catch (UnsupportedEncodingException e) {
@@ -317,18 +333,36 @@ public class Api {
                             HttpHeaderParser.parseCharset(error.networkResponse.headers));
 
                     JSONObject jsonObject = new JSONObject(jsonString);
-
-                    if (jsonObject.has("error_description")) {
-                        message = jsonObject.getString("error_description");
-                    } else if (jsonObject.has("errors")) {
-                        JSONArray errors = jsonObject.getJSONArray("errors");
-                        if (errors.length() > 0) {
-                            message = errors.getString(0);
-                        }
-                    }
+                    message = getErrorMessage(jsonObject);
                 } catch (Exception e) {
                     // ignore
                 }
+            }
+
+            return message;
+        }
+
+        String getErrorMessage(JSONObject response) {
+            String message = null;
+
+            try {
+                if (response.has("error_description")) {
+                    message = response.getString("error_description");
+                } else if (response.has("errors")) {
+                    try {
+                        JSONArray errors = response.getJSONArray("errors");
+                        if (errors.length() > 0) {
+                            message = errors.getString(0);
+                        }
+                    } catch (JSONException je) {
+                        JSONObject errors = response.getJSONObject("errors");
+                        JSONArray names = errors.names();
+                        String name = names.getString(0);
+                        message = errors.getString(name);
+                    }
+                }
+            } catch (Exception e) {
+                // ignore
             }
 
             return message;
@@ -385,8 +419,72 @@ public class Api {
     }
 
     public static class PostRequest extends Request {
+
+        private Map<String, InputStreamBody> mFiles = new HashMap<>();
+        private MultipartEntityBuilder mBodyBuilder = null;
+        private HttpEntity mBuiltBody = null;
+
         public PostRequest(String url, Map<String, String> params) {
             super(Method.POST, makeUrl(Method.POST, url, params), params);
+        }
+
+        @Override
+        public void start() {
+            super.start();
+
+            if (BuildConfig.DEBUG) {
+                for (String key : mFiles.keySet()) {
+                    Log.v(getTag().toString(), "Request[" + key + "](file)=" + mFiles.get(key).getFilename());
+                }
+            }
+        }
+
+        @Override
+        public String getBodyContentType() {
+            if (mFiles.size() == 0) {
+                return super.getBodyContentType();
+            }
+
+            return mBuiltBody.getContentType().getValue();
+        }
+
+        @Override
+        public byte[] getBody() throws AuthFailureError {
+            if (mFiles.size() == 0) {
+                return super.getBody();
+            }
+
+            if (mBuiltBody == null) {
+                mBodyBuilder = MultipartEntityBuilder.create();
+                mBodyBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+                for (Map.Entry<String, String> param : mParams.entrySet()) {
+                    mBodyBuilder.addTextBody(param.getKey(), param.getValue());
+                }
+
+                for (Map.Entry<String, InputStreamBody> file : mFiles.entrySet()) {
+                    mBodyBuilder.addPart(file.getKey(), file.getValue());
+                }
+
+                mBuiltBody = mBodyBuilder.build();
+            }
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            try {
+                mBuiltBody.writeTo(bos);
+            } catch (IOException e) {
+                Log.e(getTag().toString(), e.toString());
+            }
+
+            return bos.toByteArray();
+        }
+
+        void addFile(String key, String fileName, InputStream inputStream) throws IllegalAccessException {
+            if (mBuiltBody != null) {
+                throw new IllegalAccessException("Cannot addFile after body has been built.");
+            }
+
+            mFiles.put(key, new InputStreamBody(inputStream, fileName));
         }
     }
 

android_demo/app/build.gradle
@@ -3,16 +3,16 @@ apply plugin: 'com.google.gms.google-services'
 apply from: 'config.gradle'
 
 android {
-    compileSdkVersion 22
-    buildToolsVersion "22.0.1"
+    compileSdkVersion 23
+    buildToolsVersion "23.0.1"
 
     defaultConfig {
         applicationId "com.xfrocks.api.androiddemo"
         resValue "string", "app_name", "Android Demo"
 
         minSdkVersion 15
-        targetSdkVersion 22
-        versionCode 2
+        targetSdkVersion 23
+        versionCode 3
         versionName "1.0.0"
     }
 
@@ -22,16 +22,25 @@ android {
             proguardFiles getDefaultProguardFile('proguard-android.txt'), 'proguard-rules.pro'
         }
     }
+
+    packagingOptions {
+        // this is required for httpcore/httpmime
+        exclude 'META-INF/DEPENDENCIES'
+        exclude 'META-INF/NOTICE'
+        exclude 'META-INF/LICENSE'
+    }
 }
 
 dependencies {
     compile fileTree(dir: 'libs', include: ['*.jar'])
-    compile 'com.android.support:appcompat-v7:22.2.0'
-    compile 'com.google.android.gms:play-services:7.5.0'
-    compile 'com.android.support:design:22.2.0'
-    compile 'com.mcxiaoke.volley:library:1.0.16'
+    compile 'com.android.support:appcompat-v7:23.0.1'
+    compile 'com.google.android.gms:play-services:8.1.0'
+    compile 'com.android.support:design:23.0.1'
+    compile 'com.mcxiaoke.volley:library:1.0.19'
     compile 'com.facebook.android:facebook-android-sdk:4.1.0'
     compile('com.twitter.sdk.android:twitter:1.6.0@aar') {
         transitive = true;
     }
+    compile 'org.apache.httpcomponents:httpcore:4.3.2'
+    compile 'org.apache.httpcomponents:httpmime:4.3.5'
 }

android_demo/app/src/main/AndroidManifest.xml
@@ -10,6 +10,7 @@
     <uses-permission android:name="android.permission.WAKE_LOCK" />
     <uses-permission android:name="com.google.android.c2dm.permission.RECEIVE" />
     <uses-permission android:name="android.permission.USE_CREDENTIALS" />
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
 
     <permission
         android:name="${applicationId}.permission.C2D_MESSAGE"

android_demo/app/src/main/java/com/xfrocks/api/androiddemo/App.java
@@ -1,10 +1,14 @@
 package com.xfrocks.api.androiddemo;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.Uri;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+
+import java.io.File;
 
 public class App extends android.app.Application {
 
@@ -56,6 +60,10 @@ public class App extends android.app.Application {
 
     public synchronized static App getInstance() {
         return sInstance;
+    }
+
+    public static Uri getAvatarUri(Context context) {
+        return Uri.fromFile(new File(context.getExternalFilesDir(null), "avatar.jpg"));
     }
 
 }

android_demo/app/src/main/java/com/xfrocks/api/androiddemo/MainActivity.java
@@ -1,10 +1,15 @@
 package com.xfrocks.api.androiddemo;
 
 import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -13,28 +18,37 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.xfrocks.api.androiddemo.persist.Row;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        View.OnClickListener {
 
     public static final String EXTRA_ACCESS_TOKEN = "access_token";
     public static final String EXTRA_URL = "url";
     private static final String STATE_NAVIGATION_ROWS = "navigation_rows";
     private static final String STATE_USER = "user";
+    private static final int RC_SELECT_AVATAR = 1;
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
@@ -43,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView mNavigationView;
     private ImageView mHeaderImg;
     private TextView mHeaderTxt;
+    private ProgressBar mHeaderProgress;
 
     private Api.AccessToken mAccessToken;
     private ArrayList<Row> mNavigationRows = new ArrayList<>();
@@ -67,9 +82,48 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
         mHeaderImg = (ImageView) findViewById(R.id.header_img);
+        mHeaderImg.setOnClickListener(this);
         mHeaderTxt = (TextView) findViewById(R.id.header_txt);
+        mHeaderProgress = (ProgressBar) findViewById(R.id.header_progress);
         mNavigationView.setNavigationItemSelectedListener(this);
         mDrawerLayout.openDrawer(mNavigationView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RC_SELECT_AVATAR:
+                if (resultCode == RESULT_OK) {
+                    Uri avatarUri = null;
+                    if (data != null) {
+                        avatarUri = data.getData();
+                    }
+                    if (avatarUri == null) {
+                        avatarUri = App.getAvatarUri(this);
+                    }
+
+                    if (avatarUri != null) {
+                        try {
+                            InputStream inputStream = getContentResolver().openInputStream(avatarUri);
+
+                            String fileName = "avatar.jpg";
+                            String mimeType = getContentResolver().getType(avatarUri);
+                            if (mimeType != null) {
+                                fileName = String.format("avatar.%s", mimeType.substring(mimeType.indexOf("/") + 1));
+                            }
+                            new UsersMeAvatarRequest(mAccessToken, fileName, inputStream).start();
+
+                            mHeaderImg.setImageURI(avatarUri);
+                            mHeaderProgress.setVisibility(View.VISIBLE);
+                        } catch (FileNotFoundException e) {
+                            Log.e(getClass().getSimpleName(), e.toString());
+                        }
+                    }
+                }
+                break;
+        }
     }
 
     @Override
@@ -154,6 +208,34 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public void onClick(View v) {
+        if (v == mHeaderImg) {
+            Intent pickIntent = new Intent();
+            pickIntent.setType("image/*");
+            pickIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+            Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Uri avatarUri = App.getAvatarUri(this);
+            List<Intent> cameraIntents = new ArrayList<>();
+            PackageManager packageManager = getPackageManager();
+            List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+            for (ResolveInfo res : listCam) {
+                final Intent intent = new Intent(captureIntent);
+                intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+                intent.setPackage(res.activityInfo.packageName);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, avatarUri);
+                cameraIntents.add(intent);
+            }
+
+            Intent chooserIntent = Intent.createChooser(pickIntent, getString(R.string.avatar_pick_image));
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                    cameraIntents.toArray(new Intent[cameraIntents.size()]));
+
+            startActivityForResult(chooserIntent, RC_SELECT_AVATAR);
+        }
+    }
+
+    @Override
     public void onBackPressed() {
         if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
             getSupportFragmentManager().popBackStack();
@@ -195,6 +277,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             mHeaderImg.setImageDrawable(null);
             mHeaderTxt.setText("");
         }
+
+        mHeaderProgress.setVisibility(View.GONE);
     }
 
     public void setTheProgressBarVisibility(boolean visible) {
@@ -277,6 +361,41 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Fragment fragment = DataSubFragment.newInstance(null, rows);
                 MainActivity.this.addFragmentToBackStack(fragment, true);
             }
+        }
+    }
+
+    private class UsersMeAvatarRequest extends Api.PostRequest {
+        public UsersMeAvatarRequest(Api.AccessToken at, String fileName, InputStream inputStream) {
+            super(Api.URL_USERS_ME_AVATAR, new Api.Params(at));
+
+            try {
+                addFile(Api.URL_USERS_ME_AVATAR_PARAM_AVATAR, fileName, inputStream);
+            } catch (IllegalAccessException e) {
+                Log.e(getTag().toString(), e.toString());
+            }
+        }
+
+        @Override
+        protected void onSuccess(JSONObject response) {
+            String message = getErrorMessage(response);
+
+            if (message != null) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        void onError(VolleyError error) {
+            String message = getErrorMessage(error);
+
+            if (message != null) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        void onComplete() {
+            new UsersMeRequest(mAccessToken).start();
         }
     }
 

android_demo/app/src/main/res/layout/drawer_header.xml
@@ -9,14 +9,26 @@
     android:padding="@dimen/activity_horizontal_margin"
     android:theme="@style/ThemeOverlay.AppCompat.Dark">
 
-    <ImageView
-        android:id="@+id/header_img"
+    <RelativeLayout
         android:layout_width="match_parent"
         android:layout_height="0dp"
-        android:layout_weight="1"
-        android:contentDescription="@string/avatar"
-        android:scaleType="fitCenter"
-        tools:src="@drawable/avatar_l" />
+        android:layout_weight="1">
+
+        <ImageView
+            android:id="@+id/header_img"
+            android:layout_width="match_parent"
+            android:layout_height="match_parent"
+            android:contentDescription="@string/avatar"
+            android:scaleType="fitCenter"
+            tools:src="@drawable/avatar_l" />
+
+        <ProgressBar
+            android:id="@+id/header_progress"
+            android:layout_width="match_parent"
+            android:layout_height="match_parent"
+            android:visibility="gone"
+            tools:visibility="visible" />
+    </RelativeLayout>
 
     <TextView
         android:id="@+id/header_txt"

android_demo/app/src/main/res/values/strings.xml
@@ -7,6 +7,8 @@
     <string name="navigation_drawer_close">Close navigation drawer</string>
 
     <string name="avatar">Avatar</string>
+    <string name="avatar_pick_image">Take new photo or pick one to upload as avatar</string>
+
     <string name="error_no_access_token">No access token, try restarting the app?</string>
     <string name="are_you_sure_quit">Are you sure you want to quit?</string>
 </resources>

android_demo/build.gradle
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:1.2.3'
+        classpath 'com.android.tools.build:gradle:1.3.0'
         classpath 'com.google.gms:google-services:1.3.0-beta1'
     }
 }

commit 3fad84455eb068f0bd575728553b8d5291c839d7

[android_demo] fixed bug authorize via web introduced by 4e5a0fe

android_demo/app/src/main/java/com/xfrocks/api/androiddemo/Api.java
@@ -188,7 +188,7 @@ public class Api {
             String authorizeRedirectUri = makeAuthorizeRedirectUri(redirectTo);
             String encodedRedirectTo = "";
             if (authorizeRedirectUri != null) {
-                URLEncoder.encode(authorizeRedirectUri, "UTF-8");
+                encodedRedirectTo = URLEncoder.encode(authorizeRedirectUri, "UTF-8");
             }
 
             return String.format(